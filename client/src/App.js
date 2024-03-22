1d80dcfed3b40f24d7133b962501f75eb7e3c158
lasithcodeolima <162966279+lasithcodeolima@users.noreply.github.com>
2024-03-22 15:14:35 +0530

    added app and version

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Footer from './Components/Footer';
 import Content from './Components/Content';
 
 function App() {
+  const appName= 'codeolima'
+  const version = '1.0.0'
   return (
     <div className="App">
       <Header/>
---
+++ client/src/Components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+      Version : 1.0.0
+    </div>
+  )
+}
