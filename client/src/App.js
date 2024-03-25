96998e2fa2a03ffc3652b71c83a10cf8f2d8d288
lasithcodeolima <162966279+lasithcodeolima@users.noreply.github.com>
2024-03-25 20:37:23 +0530

    add browser routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,39 @@
-
-import './App.css';
-import Header from './Components/Header';
-import Footer from './Components/Footer';
-import Content from './Components/Content';
-import Home from './Components/Home';
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Content from "./Components/Content";
+import Home from "./Components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AboutUs from "./Components/AboutUs";
 
 function App() {
   const appName = "Codeolima Web App";
   const version = process.env.REACT_APP_VERSION;
-
+  <>
+    <Header appName={appName} />
+    <Content />
+    <Footer version={version} />
+  </>;
   return (
     <div>
-      <Header appName={appName}/>
+      <BrowserRouter>
+        <Routes>
 
-      {/*Parent props and mutiple props*/}
-      <Home projName="rnd-mern" org="Codeolima">
-      {/*children props*/}
-      <p>this is the web application.</p><br/>
-      <p>Welocome to Codeolima.</p>
-      </Home>
+          {/*Parent props and mutiple props*/}
+          <Route
+            path="/"
+            element={
+              <Home projName="rnd-mern" org="Codeolima">
+                {/*children props*/} <p>this is the web application.</p>
+                <br />
+                <p>Welocome to Codeolima.</p>
+              </Home>
+            }
+          ></Route>
 
-
-      <Content/>
-      <Footer version={version}/>
+          <Route path="/AboutUs" element={<AboutUs/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
