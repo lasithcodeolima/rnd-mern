13287e71076f1355413b10a6afa5c96c40b127ae
lasithcodeolima <162966279+lasithcodeolima@users.noreply.github.com>
2024-04-02 16:35:53 +0530

    add Product delete API endpoint

--- server/routes/product.js
+++ server/routes/product.js
@@ -23,4 +23,30 @@ router.get('/:id', function(req, res, next) {
 
 });
 
+//create product
+// Product Update by Id 
+
+// Delete Product
+// Delete Product by Id
+router.delete('/:id',function(req,res,next){
+
+    try {
+        const id = req.params.id;
+        // Start : Should replace by Actual DB Query 
+        const product = DUMMY_PRODUCT_LIST.find((item)=>item._id === id)
+        //End
+        if(product){
+            // Start : Should replace by Actual DB Query 
+            const index = DUMMY_PRODUCT_LIST.findIndex((item) => item._id === product._id)
+            DUMMY_PRODUCT_LIST.splice(index,1)
+            //End
+            return res.status(200).json()
+        }else{
+            return res.status(400).json()
+        }
+    } catch (err) {
+        return res.status(500).json()
+    }
+});
+
 module.exports = router;
